58235ca9585304830901434b38cc58de9319635e
geokats <geo.katsogiannis@gmail.com>
2021-09-17 16:25:11 +0300

    Move table embedding creation to separate function

--- main.py
+++ main.py
@@ -20,6 +20,52 @@ def parse_args():
 def wikisql_table_to_df(table):
     return pd.DataFrame(columns=table['header'], data=table['rows'])
 
+def create_table_emb(tdf):
+    """
+    Use EmbDI to create table embeddings for a WikISQL table
+
+        Parameters:
+            tdf : The Dataframe of a WikiSQL table
+    """
+    edgefile = "example.edgelist"
+    prefixes = ['3#__tn', '3$__tt', '5$__idx', '1$__cid']
+    info = None
+
+    edgelist = EdgeList(tdf, edgefile, prefixes, info, flatten=True)
+
+    # Default parameters
+    configuration = {
+        'walks_strategy': 'basic',
+        'walks_file' : 'example.walks',
+        'flatten': 'all',
+        'input_file': 'example.edgelist',
+        'n_sentences': 'default',
+        'sentence_length': 10,
+        'write_walks': True,
+        'intersection': False,
+        'backtrack': True,
+        'output_file': 'example',
+        'repl_numbers': False,
+        'repl_strings': False,
+        'follow_replacement': False,
+        'mlflow': False
+    }
+
+    prefixes, edgelist = read_edgelist(configuration['input_file'])
+
+    graph = graph_generation(configuration, edgelist, prefixes, dictionary=None)
+    if configuration['n_sentences'] == 'default':
+        #  Compute the number of sentences according to the rule of thumb.
+        configuration['n_sentences'] = graph.compute_n_sentences(int(configuration['sentence_length']))
+    walks = random_walks_generation(configuration, graph)
+
+
+    learn_embeddings("example.emb", "example.walks", True, 100, 15,
+                     training_algorithm='fasttext',
+                     learning_method='skipgram', sampling_factor=0.001
+                    )
+
+
 if __name__ == '__main__':
     args = parse_args()
 
@@ -30,41 +76,4 @@ if __name__ == '__main__':
         table = json.loads(line)
         tdf = wikisql_table_to_df(table)
         print(tdf)
-
-        edgefile = "example.edgelist"
-        prefixes = ['3#__tn', '3$__tt', '5$__idx', '1$__cid']
-        info = None
-
-        edgelist = EdgeList(tdf, edgefile, prefixes, info, flatten=True)
-
-        # Default parameters
-        configuration = {
-            'walks_strategy': 'basic',
-            'walks_file' : 'example.walks',
-            'flatten': 'all',
-            'input_file': 'example.edgelist',
-            'n_sentences': 'default',
-            'sentence_length': 10,
-            'write_walks': True,
-            'intersection': False,
-            'backtrack': True,
-            'output_file': 'example',
-            'repl_numbers': False,
-            'repl_strings': False,
-            'follow_replacement': False,
-            'mlflow': False
-        }
-
-        prefixes, edgelist = read_edgelist(configuration['input_file'])
-
-        graph = graph_generation(configuration, edgelist, prefixes, dictionary=None)
-        if configuration['n_sentences'] == 'default':
-            #  Compute the number of sentences according to the rule of thumb.
-            configuration['n_sentences'] = graph.compute_n_sentences(int(configuration['sentence_length']))
-        walks = random_walks_generation(configuration, graph)
-
-
-        learn_embeddings("example.emb", "example.walks", True, 100, 15,
-                         training_algorithm='fasttext',
-                         learning_method='skipgram', sampling_factor=0.001
-                        )
+        create_table_emb(tdf)
